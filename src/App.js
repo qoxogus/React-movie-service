453c2383a98b77ee5837d3bc460f0ae138c539ba
qoxogus <a01034367055@gmail.com>
2021-07-23 01:31:04 +0900

    [ADD] 동적 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-function Food({ favorite }) { // == property.fav
-  return <h1>I like {favorite}</h1>;
+function Food({ name, pricture }) { // == property.fav
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src={pricture} />
+  </div>
+  )
 }
+const foodILike = [
+  {
+  name: "Kimchi",
+  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+  name: "samgyetang",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Food favorite="kimchi" />
-      <Food favorite="ramen" />
-      <Food favorite="samgiopsal" />
-      <Food favorite="chukumi" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} pricture={dish.image} />
+      ))}
     </div>
   );
 }
