cfafb79de30254c2cfed1dd2a903940b17fbe3c7
qoxogus <a01034367055@gmail.com>
2021-07-23 01:56:19 +0900

    [ADD] propTypes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 5
   },
   {
     id: 2,
     name: "Samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 4.9
   }
 ];
 
@@ -17,20 +20,27 @@ const foodILike = [
 //   return <Food name={dish.name} picture={dish.image} />;
 // }
 
-function Food({ name, picture }) { // == property.fav
+function Food({ name, picture, rating }) { // == property.fav
   return (
   <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>
   )
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 function App() {
   return (
     <div>
       {foodILike.map(dish => ( //renderFood로 대체가능
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
   );
