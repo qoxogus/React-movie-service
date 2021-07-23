9d674c2f28dc63d12a12ece649571de72379d1ca
qoxogus <a01034367055@gmail.com>
2021-07-24 00:08:26 +0900

    [ADD] class components and state

--- src/App.js
+++ src/App.js
@@ -1,49 +1,31 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating: 4.9
+class App extends React.Component {
+
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    console.log("add");
+  };
+
+  minus = () => {
+    console.log("minus");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
 
-// function renderFood(dish) {
-//   return <Food name={dish.name} picture={dish.image} />;
-// }
 
-function Food({ name, picture, rating }) { // == property.fav
-  return (
-  <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => ( //renderFood로 대체가능
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
 }
 
 export default App;
